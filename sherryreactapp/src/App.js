1991de218c7930ac39d6f20726e8264aebbc72d2
Shaharyar-54 <shaharyarn054@gmail.com>
2024-01-30 15:14:12 +0500

    first commit

---
+++ sherryreactapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Resturant  from './component/Basics/Resturant';
+// import UseState from './component/Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+import Temp from './component/weather/temp';
+const  App=()=>{
+  return(
+  <>
+ <Temp/>
+    </>
+    )
+}
+
+
+export default App;
